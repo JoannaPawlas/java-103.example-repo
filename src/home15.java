cb0a0266f8f72ec40ff4ebbf08c0423515d8a5c1
Joanna Pawlas <joanna_pawlas@op.pl>
2021-11-03 16:34:53 +0100

    adding still in progress home15

---
+++ src/home15.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class home15 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj kilka liczb: ");
+        int number = scanner.nextInt();
+        int [] numbers = new int[number];
+        for (int i = 0; i < number; i++){
+            numbers[i] = scanner.nextInt();
+        }
+
+
+        }
+    public static void howMany (int [] numbers){
+
+
+    }
+}
